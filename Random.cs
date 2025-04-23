fb763b29f205cd1bac2d8f2347d21b4912425b71
Nejc855 <nejccokan855@gmail.com>
2025-04-23 09:01:19 +0200

    ss
    ss

--- Random.cs
+++ Random.cs
@@ -4,13 +4,20 @@ using Unity.Random;
 public class  : MonoBehaviour
 {
    int i, je = 0;
-   public float cas;
+   public float cas, casD;
+   public gameObject objekt;
 
     void Detonacija() {
         i = Random.Range(1, 3);
         if(i == 1) {
             //.. tu se duplicirajo
         }
+    }
+
+    void kloniranje()
+    {
+       Instantiate(objekt, gameObject.transform.position, Quaternion.identity);
+       Destroy(objekt, casD);
     }
 
     void Update() {
