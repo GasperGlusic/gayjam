4ea8dac4350fd9194927f0ca98b08e35e624f6d0
Nejc855 <nejccokan855@gmail.com>
2025-04-23 08:31:37 +0200

    sqs
    sw

--- Time.cs
+++ Time.cs
@@ -1,33 +1,33 @@
-using UnityEngine;
-using UnityEngine.SceneManagement;
-using TMPro;
+    using UnityEngine;
+    using UnityEngine.SceneManagement;
+    using TMPro;
 
-public class Timer : MonoBehaviour
-{
-   int je = 0;
-   public int cas;
-   public float speed;
-   TextMeshProUGUI text; 
+    public class Timer : MonoBehaviour
+    {
+    int je = 0;
+    public int cas;
+    public float speed;
+    TextMeshProUGUI text; 
 
-    void Start() {
-        TextMeshProUGUI text = gameObject.GetComponent<TextMeshProUGUI>();
+        void Start() {
+            text = gameObject.GetComponent<TextMeshProUGUI>();
+        }
+
+    void Timr() {
+        cas--;
+        je = 0;
     }
 
-   void Timr() {
-    cas--;
-    je = 0;
-   }
+    void Update() {
+        text.text = (cas/60).ToString() + " : " + (cas%60).ToString();
+        
+        if(je == 0) {
+            je = 1;
+            Invoke("Timr", speed);
+        }
 
-   void Update() {
-    text.text = (cas/60).ToString() + " : " + (cas%60).ToString();
-    
-    if(je == 0) {
-        je = 1;
-        Invoke("Timr", speed);
+        if(cas == 0) {
+            //..load end in prikazi score
+        }
     }
-
-    if(cas == 0) {
-        //..load end in prikazi score
     }
-   }
-}
